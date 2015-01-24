86d2b9bf98b3da98308be0378e5cd61b4aab1d87
Clint <lcrawford31@gatech.edu>
2015-01-24 08:28:18 -0500

    Initial Project Commit

---
+++ LocalizationIntro/GettingStarted/One-One.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 24, 2015
+
+@author: Wastedyu6
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+    print("Veula")
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
